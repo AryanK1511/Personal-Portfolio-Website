1c7b9e2c4ce6990d910708764b00ffce106ae8f6
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-05 16:28:16 -0500

    Added particle background and text animation

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: ['Software Developer.', 'Web Developer.', 'Computer Science Enthusiast.'],
+    typeSpeed: 40,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false
+});
