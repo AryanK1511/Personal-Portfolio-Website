f8f27622bda1b606ec8d4c661b366dd57d7a23d5
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-06 17:41:18 -0500

    Dynamically loading project information

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,30 @@
+// Projects information
+let projects = [
+    {
+        id: 1,
+        title: "BookWorm Web App",
+        subtitle: "CRUD web application built using Node.js, Express.js, EJS and mongoDB.",
+        imgSrc: "./Assets/Images/BookWorm-Logo.png",
+        repoLink: "https://github.com/AryanK1511/Bookworm-Web-App"
+    },
+
+    {
+        id: 2,
+        title: "Sort Visualizer",
+        subtitle: "Visualizer which allows user to observe the working of Bubble Sort, Insertion Sort and Selection Sort.",
+        imgSrc: "./Assets/Images/Sort-Logo.png",
+        repoLink: "https://github.com/AryanK1511/Sort-Visualizer"
+    },
+
+    {
+        id: 3,
+        title: "Portfolio Website",
+        subtitle: "My very own portfolio website which contains my personal and contact information.",
+        imgSrc: "./Assets/Images/Portfolio-Logo.png",
+        repoLink: "https://github.com/AryanK1511/Personal-Portfolio-Website"
+    }
+]
+
 // Fetching all the elements
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
@@ -5,8 +32,8 @@ const articles = document.querySelectorAll(".content");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message");
-const success = document.querySelector("#success");
 const errorNodes = document.querySelectorAll(".error");
+const projectsDisplay = document.getElementById("projects-display");
 
 // ========== Configuring about page tabs section ==========
 about.addEventListener("click", function (e) {
@@ -28,6 +55,22 @@ about.addEventListener("click", function (e) {
     element.classList.add("active");
   }
 });
+
+// ========== Rendering projects dynamically ==========
+projectsDisplay.innerHTML = "";
+projects.forEach((project) => {
+    console.log(project);
+    projectsDisplay.innerHTML += `<div class="col-lg-6 col-xl-4 my-4">
+        <div class="card h-100 mx-3">
+            <img class="card-img-top mx-auto" src="${project.imgSrc}" alt="Project Image">
+            <div class="card-body">
+                <h5 class="card-title">${project.title}</h5>
+                <h6 class="card-subtitle mb-2">${project.subtitle}</h6>
+                <p class="card-text text-muted"><a href="${project.repoLink}" target="_blank"><i class="uil uil-github"></i></a></p>
+            </div>
+        </div>
+    </div>`;
+})
 
 // ========== FORM VALIDATION ==========
 let error = false;
@@ -54,7 +97,7 @@ function validateForm() {
     }
 }
 
-// Clear error and success messages
+// Clear error messages
 function clearMessages() {
     error = false;
     for (let i = 0; i < errorNodes.length; i++) {
