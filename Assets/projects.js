9b7a5770b12080df554be4d84f6a102a1cfdb107
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-06 13:49:56 -0500

    Created projects.js file

---
+++ Assets/projects.js
@@ -0,0 +1,25 @@
+projects = [
+    {
+        id: 1,
+        title: "BookWorm Web App",
+        subtitle: "CRUD web application built using Node.js, Express.js, EJS and mongoDB.",
+        imgSrc: "./Assets/Images/BookWorm-Logo.png",
+        repoLink: "https://github.com/AryanK1511/Bookworm-Web-App"
+    },
+
+    {
+        id: 2,
+        title: "Sort Visualizer",
+        subtitle: "Visualizer which allows user to observe the working of Bubble Sort, Insertion Sort and Selection Sort.",
+        imgSrc: "./Assets/Images/Sort-Logo.png",
+        repoLink: "https://github.com/AryanK1511/Sort-Visualizer"
+    },
+
+    {
+        id: 3,
+        title: "Portfolio Website",
+        subtitle: "My very own portfolio website which contains my personal and contact information.",
+        imgSrc: "./Assets/Images/Portfolio-Logo.png",
+        repoLink: "https://github.com/AryanK1511/Personal-Portfolio-Website"
+    }
+]
